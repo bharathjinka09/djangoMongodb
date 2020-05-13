26c3cf4eb3afae9e82a1643334b6f77eab54f81d
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-13 15:03:14 +0530

    Added google social login redirect to home

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('accounts/profile/', views.home),
+    path('not_logged_in', views.not_logged_in),
 ]
